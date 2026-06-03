9c3257e03cb9b355efdced336ef304eaf5f31a67
anuj-codess <anujanujsharma610@gmail.com>
2026-06-03 23:57:07 +0530

    dsa

---
+++ EarliestFinishTime.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration,
+                                  int[] waterStartTime, int[] waterDuration) {
+
+        int ans = Integer.MAX_VALUE;
+
+        // Land -> Water
+        for (int i = 0; i < landStartTime.length; i++) {
+
+            int landFinish = landStartTime[i] + landDuration[i];
+
+            for (int j = 0; j < waterStartTime.length; j++) {
+
+                int waterBegin = Math.max(landFinish, waterStartTime[j]);
+
+                int finish = waterBegin + waterDuration[j];
+
+                ans = Math.min(ans, finish);
+            }
+        }
+
+        // Water -> Land
+        for (int i = 0; i < waterStartTime.length; i++) {
+
+            int waterFinish = waterStartTime[i] + waterDuration[i];
+
+            for (int j = 0; j < landStartTime.length; j++) {
+
+                int landBegin = Math.max(waterFinish, landStartTime[j]);
+
+                int finish = landBegin + landDuration[j];
+
+                ans = Math.min(ans, finish);
+            }
+        }
+
+        return ans;
+    }
+}
